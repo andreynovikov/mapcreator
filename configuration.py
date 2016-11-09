7fe0c6857565a618190f1db7d01d883d5547d1f5
Andrey Novikov <novikov@gmail.com>
2016-11-09 13:17:38 +0300

    Add osmconvert options

--- configuration.py
+++ configuration.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 OSMOSIS_PATH = '/gis/osmosis/package/bin/osmosis'
+OSMCONVERT_PATH = '/usr/bin/osmconvert'
 ZOOM_INTERVAL = '10,8,11,14,12,20'
 MAP_START_ZOOM = '8'
 PREFERRED_LANGUAGES = 'en,de,ru'
-TAG_MAPPING = '/var/gis/tag-mapping.xml'
+TAG_MAPPING = '/gis/tag-mapping.xml'
 
-MAP_TARGET_PATH = '/var/gis/maps'
+SOURCE_PBF = '/gis/planet-161031.osm.pbf'
+MAP_TARGET_PATH = '/gis/maps'
 LOGGING_PATH = '/var/log/mapcreator'
-DATA_PATH = '/var/gis/data'
+DATA_PATH = '/gis/data'
 
 MAP_DOWNLOAD_LOG = '/var/log/nginx/maps.log'
 STATS_DB_DSN = 'dbname=gis'
