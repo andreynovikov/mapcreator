8294ed808d4115f2eda06dec0c43c56f5dbedeef
Andrey Novikov <novikov@gmail.com>
2017-07-11 10:23:15 +0200

    Proper field name

--- setup/water2pgsql.sh
+++ setup/water2pgsql.sh
@@ -3,5 +3,5 @@
 # After unpacking the contents of each zip archive, pipe this script to psql.
 #
 
-shp2pgsql -dID -s 3857 -W UTF-8 -g geometry "data/water-polygons-split-3857/water_polygons.shp" "osmd_water"
-shp2pgsql -dID -s 3857 -W UTF-8 -g geometry "data/simplified-water-polygons-complete-3857/simplified_water_polygons.shp" "osmd_water_z7"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-split-3857/water_polygons.shp" "osmd_water"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/simplified-water-polygons-complete-3857/simplified_water_polygons.shp" "osmd_water_z7"
