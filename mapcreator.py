9a0aa9a1c9676384e3ffefdd03db7543839fc5e8
Andrey Novikov <novikov@gmail.com>
2017-08-11 20:23:03 +0300

    Create native maps

--- mapcreator.py
+++ mapcreator.py
@@ -14,16 +14,17 @@ import configuration
 
 class MapCreator:
 
-    def __init__(self, data_dir, dry_run=False, verbose=False):
+    def __init__(self, data_dir, dry_run=False, forbid_interactive=False):
         self.dry_run = dry_run
-        self.verbose = verbose
+        self.forbid_interactive = forbid_interactive
         self.logger = logging.getLogger("mapcreator")
         self.data_dir = data_dir
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
-        self.mapWriter = mapwrite.MapWriter(self.data_dir, self.dry_run, self.verbose)
 
-        index = open(configuration.MAP_TARGET_PATH + '/index', 'r+b')
+        self.mapWriter = mapwrite.MapWriter(self.data_dir, self.dry_run, self.forbid_interactive)
+
+        index = open(configuration.MAP_TARGET_PATH + '/nativeindex', 'r+b')
         index.truncate(6*128*128)
         index.close()
 
@@ -67,12 +68,14 @@ class MapCreator:
         return None
 
 
-    def loop(self):
-        area = self.selectPopularMap(0.05, '2 weeks') or \
-               self.selectPopularMap(0.1, '1 month') or \
-               self.selectPopularMap(0.5, '2 months') or \
-               self.selectDownloadedMap('2 months') or \
-               self.selectAnyMap('4 months')
+    def loop(self, area):
+        if not area:
+            area = self.selectPopularMap(0.05, '2 weeks') or \
+                   self.selectPopularMap(0.1, '1 month') or \
+                   self.selectPopularMap(0.5, '2 months') or \
+                   self.selectDownloadedMap('2 months') or \
+                   self.selectAnyMap('4 months')
+
         if area is None:
             self.logger.debug("No maps to create")
             return
@@ -89,6 +92,12 @@ class MapCreator:
             return
         cost = int(time.time() - cost)
 
+        if map_path is None:
+            self.logger.info("Empty map, skipping")
+            if not self.dry_run:
+                self.writeIndex(area, x, y, 0, cost, 0)
+            return
+
         date = int(os.path.getmtime(map_path) / 3600 / 24)
         size = os.path.getsize(map_path)
         if not self.dry_run and size == 0:
@@ -96,9 +105,9 @@ class MapCreator:
             self.writeIndex(area, x, y, None, None, None, True)
             return
 
-        map_target_path = '{0:s}/{1:d}/{1:d}-{2:d}.map'.format(configuration.MAP_TARGET_PATH, x, y)
+        map_target_path = '{0:s}/{1:d}/{1:d}-{2:d}.mtiles'.format(configuration.MAP_TARGET_PATH, x, y)
         move_call = ["mv", map_path, map_target_path]
-        self.logger.debug("calling: %s"," ".join(move_call))
+        self.logger.info("calling: %s"," ".join(move_call))
         if not self.dry_run:
             try:
                 subprocess.check_call(move_call)
@@ -120,7 +129,7 @@ class MapCreator:
                     self.logger.debug(cur.query)
                 c.commit()
         else:
-            with open(configuration.MAP_TARGET_PATH + '/index', 'r+b') as index:
+            with open(configuration.MAP_TARGET_PATH + '/nativeindex', 'r+b') as index:
                 index.seek((x * 128 + y) * 6)
                 index.write((date).to_bytes(2, byteorder='big', signed=False))
                 index.write((size).to_bytes(4, byteorder='big', signed=False))
@@ -135,25 +144,27 @@ class MapCreator:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='MapTrek map creation daemon')
-    parser.add_argument('-p', '--data-path', default='data', help='base path for data files')
+    parser.add_argument('-p', '--data-path', default=configuration.DATA_PATH, help='base path for data files')
     parser.add_argument('-d', '--dry-run', action='store_true', help='do not generate any files')
-    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose logging')
-    parser.add_argument('-n', '--daemonize', action='store_true', help='run as a daemon')
+    parser.add_argument('-l', '--log', default='ERROR', help='set logging verbosity')
+    parser.add_argument('-n', '--noninteractive', action='store_true', help='forbid interactive mode')
+    parser.add_argument('-z', '--daemonize', action='store_true', help='run as a daemon')
+    parser.add_argument('-a', '--area', help='create specific area')
     args = parser.parse_args()
 
-    sh = logging.StreamHandler()
+    log_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(log_level, int):
+        print("Invalid log level: %s" % args.log)
+        exit(1)
+
+    logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s - %(message)s')
     logger = logging.getLogger("mapcreator")
-    formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
-    sh.setFormatter(formatter)
-    logger.addHandler(sh)
     # during a dry run the console should receive all logs
-    if args.dry_run or args.verbose:
+    if args.dry_run:
         logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
 
     try:
-        mapCreator = MapCreator(args.data_path, args.dry_run, args.verbose)
+        mapCreator = MapCreator(args.data_path, args.dry_run, args.noninteractive)
         if args.daemonize:
             import daemon
             import lockfile
@@ -166,9 +177,6 @@ if __name__ == "__main__":
                     mapCreator.loop()
                     time.sleep(5)
         else:
-            mapCreator.loop()
+            mapCreator.loop(args.area)
     except Exception as e:
-        print("An error occurred:")
-        print(e)
-        import traceback
-        traceback.print_exc()
+        logger.exception("An error occurred:")
