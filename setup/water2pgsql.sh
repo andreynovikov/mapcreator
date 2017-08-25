63a6e5e733ba060aaa2296b2d69c49ec41a62b13
Andrey Novikov <novikov@gmail.com>
2017-08-25 20:50:26 +0300

    Use another set of generalized water polygons

--- setup/water2pgsql.sh
+++ setup/water2pgsql.sh
@@ -4,4 +4,10 @@
 #
 
 shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-split-3857/water_polygons.shp" "osmd_water"
-shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/simplified-water-polygons-complete-3857/simplified_water_polygons.shp" "osmd_water_z7"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z8.shp" "osmd_water_z8"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z7.shp" "osmd_water_z7"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z6.shp" "osmd_water_z6"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z5.shp" "osmd_water_z5"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z4.shp" "osmd_water_z4"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z3.shp" "osmd_water_z3"
+shp2pgsql -dID -s 3857 -W UTF-8 -g geom "data/water-polygons-generalized-3857/water_polygons_z2.shp" "osmd_water_z2"
