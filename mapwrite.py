6dd2874996fcbb7f9e994281f3654aed3e5571e8
Andrey Novikov <novikov@gmail.com>
2017-12-10 17:38:57 +0300

    Add pre-processors support

--- mapwrite.py
+++ mapwrite.py
@@ -31,6 +31,7 @@ from shapely.affinity import affine_transform
 import configuration
 import mappings
 from encoder import encode
+from util.core import Element
 from util.database import MTilesDatabase
 from util.geometry import wgs84_to_mercator, mercator_to_wgs84, clockwise
 from util.osm import is_area
@@ -55,52 +56,11 @@ def deep_get(dictionary, *keys):
     return dictionary
 
 
-class Element():
-    geom_type = {0: 'unknown', 1: 'node', 2: 'way', 3: 'relation'}
-
-    def __init__(self, id, geom, tags, mapping=None):
-        self.id = id
-        self.geom = geom # original geometry
-        self.tags = tags
-        self.mapping = mapping
-        self.label = None
-        self.area = None
-        self.kind = None
-        self.height = None
-        self.min_height = None
-        self.building_color = None
-        self.roof_color = None
-        self.geometry = None # tile processed temporary geometry
-
-    def osm_id(self):
-        t = 0
-        id = None
-        if self.id:
-            t = self.id & 0x0000000000000003
-            id = self.id >> 2
-        return "%s/%s" % (Element.geom_type[t], id)
-
-    def __str__(self):
-        #geom = transform(mercator_to_wgs84, self.geom)
-        #return "%s: %s\n%s\n%s\n" % (str(self.id), geom, self.tags, self.mapping)
-        return "%s: %s\n%s\n%s\n" % (self.osm_id(), self.geom.__repr__(), self.tags, self.mapping)
-
-    def clone(self, geom):
-        el = Element(self.id, geom, self.tags, self.mapping)
-        el.label = self.label
-        el.area = self.area
-        el.kind = self.kind
-        el.height = self.height
-        el.min_height = self.min_height
-        el.building_color = self.building_color
-        el.roof_color = self.roof_color
-        return el
-
-
 class OsmFilter(osmium.SimpleHandler):
     def __init__(self, elements, basemap, logger):
         super(OsmFilter, self).__init__()
         self.elements = elements
+        self.processors = set()
         self.outlines = set()
         self.logger = logger
         self.basemap = basemap
@@ -178,6 +138,8 @@ class OsmFilter(osmium.SimpleHandler):
                             mapping['zoom-max'] = m['zoom-max']
                     if 'modify-mapping' in m:
                         modifiers.add(m['modify-mapping'])
+                    if 'pre-process' in m:
+                        self.processors.add(m['pre-process'])
 
         for modifier in modifiers:
             renderable, ignorable, mapping = modifier(filtered_tags, renderable, ignorable, mapping)
@@ -264,6 +226,7 @@ class OsmFilter(osmium.SimpleHandler):
                     found = found + 1
                     element.tags['building:outline'] = 1
             self.logger.debug("    outlined %d of %d buildings" % (found, len(self.outlines)))
+        return self.processors
 
 
 class Tile():
@@ -391,7 +354,7 @@ class MapWriter:
         elements = []
         handler = OsmFilter(elements, self.basemap, self.logger)
         handler.apply_file(pbf_path)
-        handler.finish()
+        processors = handler.finish()
         del handler
         gc.collect()
 
@@ -406,6 +369,10 @@ class MapWriter:
             self.multiprocessing = total / used > 4
 
             self.timestamp = int(self.timestamp / 3600 / 24)
+
+            for processor in processors:
+                self.logger.info("    calling pre-processor %s.%s" % (processor.__module__, processor.__name__))
+                processor(elements, self.interactive)
 
             self.db = MTilesDatabase(map_path)
             if self.basemap:
@@ -559,7 +526,7 @@ class MapWriter:
             if self.interactive:
                 self.proc_progress.close()
             else:
-                self.logger.info("    finished pre-processing elements")
+                self.logger.info("    finished processing elements")
 
             gc.collect()
 
---
+++ util/core.py
@@ -0,0 +1,42 @@
+class Element():
+    geom_type = {0: 'unknown', 1: 'node', 2: 'way', 3: 'relation'}
+
+    def __init__(self, id, geom, tags, mapping=None):
+        self.id = id
+        self.geom = geom # original geometry
+        self.tags = tags
+        self.mapping = mapping
+        self.label = None
+        self.area = None
+        self.kind = None
+        self.height = None
+        self.min_height = None
+        self.building_color = None
+        self.roof_color = None
+        self.geometry = None # tile processed temporary geometry
+
+    def osm_id(self):
+        t = 0
+        id = None
+        if self.id:
+            t = self.id & 0x0000000000000003
+            id = self.id >> 2
+        return "%s/%s" % (Element.geom_type[t], id)
+
+    def __str__(self):
+        #geom = transform(mercator_to_wgs84, self.geom)
+        #return "%s: %s\n%s\n%s\n" % (str(self.id), geom, self.tags, self.mapping)
+        return "%s: %s\n%s\n%s\n" % (self.osm_id(), self.geom.__repr__(), self.tags, self.mapping)
+
+    def clone(self, geom):
+        el = Element(self.id, geom, self.tags, self.mapping)
+        el.label = self.label
+        el.area = self.area
+        el.kind = self.kind
+        el.height = self.height
+        el.min_height = self.min_height
+        el.building_color = self.building_color
+        el.roof_color = self.roof_color
+        return el
+
+
