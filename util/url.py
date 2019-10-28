2ce270d135f15f25a66ae20788b8efee3042a4e5
Andrey Novikov <novikov@gmail.com>
2019-10-28 23:07:34 +0300

    Fix url processing crashes

--- util/url.py
+++ util/url.py
@@ -1,14 +1,25 @@
 from urllib.parse import urlsplit, urlunsplit, quote
 
 
-def iri2uri(iri):
+def iri2uri(iri: str):
     """
     Convert an IRI to a URI
     """
-    (scheme, netloc, path, query, fragment) = urlsplit(iri)
+    try:
+        (scheme, netloc, path, query, fragment) = urlsplit(iri)
+    except ValueError:
+        iri = iri.strip().split(' ', 1)[0]  # second try
+        try:
+            (scheme, netloc, path, query, fragment) = urlsplit(iri)
+        except ValueError:
+            return None
+
     scheme = quote(scheme)
     if not netloc.isascii():
-        netloc = netloc.encode('idna').decode('utf-8')
+        try:
+            netloc = netloc.encode('idna').decode('utf-8')
+        except UnicodeError:
+            return None
     if path == '/':
         path = ''
     else:
