e1a7837524c98fea3a77053e9074d76b5b3e4720
Andrey Novikov <novikov@gmail.com>
2020-04-08 18:18:46 +0300

    Clean named cutlines and fix error handling

--- util/processing/cutlines.py
+++ util/processing/cutlines.py
@@ -1,5 +1,7 @@
+import logging
 from tqdm import tqdm
 from shapely.geometry import CAP_STYLE
+from shapely.errors import ShapelyError
 
 from util.core import Element
 
@@ -14,7 +16,7 @@ def process(elements, interactive):
     for element in elements:
         if 'man_made' in element.tags and element.tags['man_made'] == 'cutline':
             cutline_areas.append(element.geom.buffer(10, resolution=BUFFER_RESOLUTION, cap_style=CAP_STYLE.square))
-            if len(element.tags) == 1:
+            if len(element.tags) == 1 or (len(element.tags) == 2 and 'name' in element.tags):
                 cutlines.append(element)
         if 'natural' in element.tags and element.tags['natural'] == 'wood':
             woods.append(element)
@@ -33,12 +35,16 @@ def process(elements, interactive):
 
     for wood in woods:
         for cutline in cutline_areas:
-            geom = wood.geom.intersection(cutline)
-            if not geom.is_empty:
-                wood.geom = wood.geom.difference(cutline)
-                # this is not always true but makes map more readable
-                elements.append(Element(None, geom, {'natural': 'grassland'}, {'zoom-min': 14}))
+            try:
+                geom = wood.geom.intersection(cutline)
+                if not geom.is_empty:
+                    wood.geom = wood.geom.difference(cutline)
+                    # this is not always true but makes map more readable
+                    elements.append(Element(None, geom, {'natural': 'grassland'}, {'zoom-min': 14}))
+            except ShapelyError:
+                logging.error("failed to cut lines from %s", wood.osm_id())
         if interactive:
+            # noinspection PyUnboundLocalVariable
             progress.update()
     if interactive:
         progress.close()
