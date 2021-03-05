4757c20e5d0cd2c7c27ddd566ffdc6c7dc5b9f89
Andrey Novikov <novikov@gmail.com>
2021-03-05 23:39:04 +0300

    Flags and extra values

--- util/database.py
+++ util/database.py
@@ -15,7 +15,7 @@ PHONE_CLEANUP_PATTERN = re.compile(r'[ ()\-.]')
 TILE_INSERT_QUERY: str = 'REPLACE INTO tiles (zoom_level, tile_column, tile_row, tile_data) VALUES (?, ?, ?, ?)'
 NAME_INSERT_QUERY: str = 'REPLACE INTO names (ref, name) VALUES (?, ?)'
 FEATURE_NAME_INSERT_QUERY: str = 'REPLACE INTO feature_names (id, lang, name) VALUES (?, ?, ?)'
-FEATURE_INSERT_QUERY: str = 'REPLACE INTO features (id, kind, type, lat, lon, opening_hours, phone, wikipedia, website) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
+FEATURE_INSERT_QUERY: str = 'REPLACE INTO features (id, kind, type, lat, lon, opening_hours, phone, wikipedia, website, flags, enum1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 
 
 class MTilesDatabase:
@@ -25,7 +25,7 @@ class MTilesDatabase:
         self.namehashes: list = []
         self.db: Optional[Connection] = None
 
-    def create(self, name: str, db_type: str, version: str, timestamp: int, db_format: str, bounds=None):
+    def create(self, name: str, db_type: str, timestamp: int, db_format: str, bounds=None):
         self.db = connect(self.filename, check_same_thread=False)
         self.db.execute('PRAGMA journal_mode = OFF')
         self.db.execute('PRAGMA synchronous = NORMAL')
@@ -39,7 +39,7 @@ class MTilesDatabase:
             self.db.execute('CREATE TABLE tiles (zoom_level INTEGER NOT NULL, tile_column INTEGER NOT NULL, tile_row INTEGER NOT NULL, tile_data BLOB NOT NULL)')
             self.db.execute('CREATE TABLE names (ref INTEGER NOT NULL, name TEXT NOT NULL)')
             self.db.execute('CREATE TABLE feature_names (id INTEGER NOT NULL, lang INTEGER NOT NULL, name INTEGER NOT NULL)')
-            self.db.execute('CREATE TABLE features (id INTEGER NOT NULL, kind INTEGER, type INTEGER, lat REAL, lon REAL, opening_hours TEXT, phone TEXT, wikipedia TEXT, website TEXT)')
+            self.db.execute('CREATE TABLE features (id INTEGER NOT NULL, kind INTEGER, type INTEGER, lat REAL, lon REAL, opening_hours TEXT, phone TEXT, wikipedia TEXT, website TEXT, flags INTEGER, enum1 INTEGER)')
             self.db.execute('CREATE UNIQUE INDEX coord ON tiles (zoom_level, tile_column, tile_row)')
             self.db.execute('CREATE UNIQUE INDEX property ON metadata (name)')
             self.db.execute('CREATE UNIQUE INDEX name_ref ON names (ref)')
@@ -56,7 +56,7 @@ class MTilesDatabase:
 
         self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('name', name))
         self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('type', db_type))
-        self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('version', version))
+        self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('version', '3'))
         self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('timestamp', timestamp))
         # self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('description', description))
         self.db.execute('INSERT INTO metadata VALUES (?, ?)', ('format', db_format))
@@ -78,6 +78,13 @@ class MTilesDatabase:
 
     def putTile(self, zoom: int, x: int, y: int, content: bytes):
         self.db.execute(TILE_INSERT_QUERY, (zoom, x, y, memoryview(content)))
+
+        """
+        86M /gis/data/7/74/74-37.mtiles
+        49M /gis/data/7/74/74-37.mtiles.gz
+        memoryview(zcompress(content, 9))
+        63M /gis/data/7/74/74-37.mtiles
+        """
 
     def putName(self, name: str) -> int:
         h: int = hash64(name)
@@ -107,6 +114,8 @@ class MTilesDatabase:
         phone: Optional[str] = None
         wikipedia: Optional[str] = None
         website: Optional[str] = None
+        flags: Optional[int] = None
+        enum1: Optional[int] = None
         if label:
             if isinstance(label, list):
                 geom = transform(mercator_to_wgs84, label[0])
@@ -134,5 +143,21 @@ class MTilesDatabase:
                 wikipedia = tags['wikipedia']
             if 'website' in tags:
                 website = compress(iri2uri(tags['website']), compression_tree=WEBSITE_TREE)
+            if 'fee' in tags and tags['fee'] == 'yes':
+                if flags is None:
+                    flags = 0x00000001
+                else:
+                    flags |= 0x00000001
+            if 'wheelchair' in tags:
+                if flags is None:
+                    flags = 0x00000000
+                if tags['wheelchair'] == 'no':
+                    flags |= 0x00000002
+                elif tags['wheelchair'] == 'limited':
+                    flags |= 0x00000004
+                elif tags['wheelchair'] == 'yes':
+                    flags |= 0x00000006
+            if 'enum1' in tags:
+                enum1 = tags['enum1']
 
-        self.db.execute(FEATURE_INSERT_QUERY, (el_id, kind, el_type, lat, lon, opening_hours, phone, wikipedia, website))
+        self.db.execute(FEATURE_INSERT_QUERY, (el_id, kind, el_type, lat, lon, opening_hours, phone, wikipedia, website, flags, enum1))
