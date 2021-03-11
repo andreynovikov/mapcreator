c241fb59f6bd288fc5de2e0e9400c3b4634b10cf
Andrey Novikov <novikov@gmail.com>
2021-03-11 18:06:03 +0300

    Speedup contours import

--- setup/contours2pgsql.sh
+++ setup/contours2pgsql.sh
@@ -14,30 +14,30 @@ function process {
     wget -q $url
     if [ $? -ne 0 ]
     then
-	if [ $? -ne 8 ]
-	then
-	    echo "Failed to fetch ${url}" >&2
-	fi
-	return
+        if [ $? -ne 8 ]
+        then
+            echo "Failed to fetch ${url}" >&2
+        fi
+        return
     fi
     unzip -q $zip
     if [ $? -ne 0 ]
     then
-	echo "Failed to unzip ${zip}" >&2
-	return
+        echo "Failed to unzip ${zip}" >&2
+        return
     fi
-    args=('-D' '-s' '4326' '-W' 'UTF-8' '-g' 'geom')
+    args=('-D' '-s' '4326' '-W' 'UTF-8' '-N' 'skip' '-g' 'geom')
     (( created == 0 )) && args+=( '-d' )
     (( created == 1 )) && args+=( '-a' )
     created=1
     args+=("${shp}" "contours")
-    shp2pgsql "${args[@]}" | psql -q gis
+    shp2pgsql "${args[@]}" | grep -v "ANALYZE \"contours\";" | psql -q gis
     if [ $? -eq 0 ]
     then
-	rm -rf ${pck}
-	rm -f ${zip}
+        rm -rf ${pck}
+        rm -f ${zip}
     else
-	echo "Failed to import ${pck}" >&2
+        echo "Failed to import ${pck}" >&2
     fi
 }
 
@@ -45,11 +45,12 @@ for i in {00..90};
 do
     for j in {000..180};
     do
-	process "N${i}E${j}"
-	process "N${i}W${j}"
-	process "S${i}E${j}"
-	process "S${i}W${j}"
+        process "N${i}E${j}"
+        process "N${i}W${j}"
+        process "S${i}E${j}"
+        process "S${i}W${j}"
     done
 done
 
-psql -q gis "delete from contours where elevation is null"
+psql -q gis "DELETE FROM contours WHERE elevation IS NULL"
+psql -q gis "ANALYZE contours"
